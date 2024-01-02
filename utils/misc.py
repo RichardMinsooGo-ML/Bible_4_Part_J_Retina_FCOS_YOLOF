95a534343258f3c9fab32ce1cb3182ac1f014f61
Richard Minsoo Go <67931944+RichardMinsooGo-ML@users.noreply.github.com>
2024-01-03 08:57:03 +0900

    Add files via upload

---
+++ utils/misc.py
@@ -0,0 +1,252 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+def sigmoid_focal_loss(logits, targets, alpha=0.25, gamma=2.0, reduction='none'):
+    p = torch.sigmoid(logits)
+    ce_loss = F.binary_cross_entropy_with_logits(input=logits, 
+                                                    target=targets, 
+                                                    reduction="none")
+    p_t = p * targets + (1.0 - p) * (1.0 - targets)
+    loss = ce_loss * ((1.0 - p_t) ** gamma)
+
+    if alpha >= 0:
+        alpha_t = alpha * targets + (1.0 - alpha) * (1.0 - targets)
+        loss = alpha_t * loss
+
+    if reduction == "mean":
+        loss = loss.mean()
+
+    elif reduction == "sum":
+        loss = loss.sum()
+
+    return loss
+
+
+def sigmoid_varifocal_loss(logits, targets, alpha=0.75, gamma=2.0, reduction='none'):
+    p = torch.sigmoid(logits)
+    ce_loss = F.binary_cross_entropy_with_logits(input=logits, 
+                                                 target=targets, 
+                                                 reduction="none")
+    pos_mask = (targets > 0.).float()
+    neg_mask = (targets == 0.).float()
+    pos_loss = ce_loss * pos_mask
+    neg_loss = ce_loss * neg_mask
+
+    loss = targets * pos_loss + alpha * (p ** gamma) * neg_loss
+
+    if reduction == "mean":
+        loss = loss.mean()
+
+    elif reduction == "sum":
+        loss = loss.sum()
+
+    return loss
+
+
+def nms(dets, scores, nms_thresh=0.4):
+    """"Pure Python NMS baseline."""
+    x1 = dets[:, 0]  #xmin
+    y1 = dets[:, 1]  #ymin
+    x2 = dets[:, 2]  #xmax
+    y2 = dets[:, 3]  #ymax
+
+    areas = (x2 - x1) * (y2 - y1)
+    order = scores.argsort()[::-1]
+
+    keep = []
+    while order.size > 0:
+        i = order[0]
+        keep.append(i)
+        xx1 = np.maximum(x1[i], x1[order[1:]])
+        yy1 = np.maximum(y1[i], y1[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+        yy2 = np.minimum(y2[i], y2[order[1:]])
+
+        w = np.maximum(1e-10, xx2 - xx1)
+        h = np.maximum(1e-10, yy2 - yy1)
+        inter = w * h
+
+        # Cross Area / (bbox + particular area - Cross Area)
+        ovr = inter / (areas[i] + areas[order[1:]] - inter + 1e-10)
+        #reserve all the boundingbox whose ovr less than thresh
+        inds = np.where(ovr <= nms_thresh)[0]
+        order = order[inds + 1]
+
+    return keep
+
+
+def is_parallel(model):
+    # Returns True if model is of type DP or DDP
+    return type(model) in (nn.parallel.DataParallel, nn.parallel.DistributedDataParallel)
+
+
+def get_total_grad_norm(parameters, norm_type=2):
+    parameters = list(filter(lambda p: p.grad is not None, parameters))
+    norm_type = float(norm_type)
+    device = parameters[0].grad.device
+    total_norm = torch.norm(torch.stack([torch.norm(p.grad.detach(), norm_type).to(device) for p in parameters]),
+                            norm_type)
+    return total_norm
+
+
+def load_weight(device, model, path_to_ckpt):
+    checkpoint = torch.load(path_to_ckpt, map_location='cpu')
+    # checkpoint state dict
+    checkpoint_state_dict = checkpoint.pop("model")
+    # model state dict
+    model_state_dict = model.state_dict()
+    # check
+    for k in list(checkpoint_state_dict.keys()):
+        if k in model_state_dict:
+            shape_model = tuple(model_state_dict[k].shape)
+            shape_checkpoint = tuple(checkpoint_state_dict[k].shape)
+            if shape_model != shape_checkpoint:
+                checkpoint_state_dict.pop(k)
+        else:
+            checkpoint_state_dict.pop(k)
+            print(k)
+
+    model.load_state_dict(checkpoint_state_dict)
+    model = model.to(device).eval()
+    print('Finished loading model!')
+
+    return model
+
+
+class CollateFunc(object):
+    def __call__(self, batch):
+        targets = []
+        images = []
+        masks = []
+
+        for sample in batch:
+            image = sample[0]
+            target = sample[1]
+            mask = sample[2]
+
+            images.append(image)
+            targets.append(target)
+            masks.append(mask)
+
+        images = torch.stack(images, 0) # [B, C, H, W]
+        masks = torch.stack(masks, 0)   # [B, H, W]
+
+        return images, targets, masks
+
+
+class SinkhornDistance(torch.nn.Module):
+    r"""
+        Given two empirical measures each with :math:`P_1` locations
+        :math:`x\in\mathbb{R}^{D_1}` and :math:`P_2` locations :math:`y\in\mathbb{R}^{D_2}`,
+        outputs an approximation of the regularized OT cost for point clouds.
+        Args:
+        eps (float): regularization coefficient
+        max_iter (int): maximum number of Sinkhorn iterations
+        reduction (string, optional): Specifies the reduction to apply to the output:
+        'none' | 'mean' | 'sum'. 'none': no reduction will be applied,
+        'mean': the sum of the output will be divided by the number of
+        elements in the output, 'sum': the output will be summed. Default: 'none'
+        Shape:
+            - Input: :math:`(N, P_1, D_1)`, :math:`(N, P_2, D_2)`
+            - Output: :math:`(N)` or :math:`()`, depending on `reduction`
+    """
+
+    def __init__(self, eps=1e-3, max_iter=100, reduction='none'):
+        super(SinkhornDistance, self).__init__()
+        self.eps = eps
+        self.max_iter = max_iter
+        self.reduction = reduction
+
+    def forward(self, mu, nu, C):
+        u = torch.ones_like(mu)
+        v = torch.ones_like(nu)
+
+        # Sinkhorn iterations
+        for i in range(self.max_iter):
+            v = self.eps * \
+                (torch.log(
+                    nu + 1e-8) - torch.logsumexp(self.M(C, u, v).transpose(-2, -1), dim=-1)) + v
+            u = self.eps * \
+                (torch.log(
+                    mu + 1e-8) - torch.logsumexp(self.M(C, u, v), dim=-1)) + u
+
+        U, V = u, v
+        # Transport plan pi = diag(a)*K*diag(b)
+        pi = torch.exp(
+            self.M(C, U, V)).detach()
+        # Sinkhorn distance
+        cost = torch.sum(
+            pi * C, dim=(-2, -1))
+        return cost, pi
+
+    def M(self, C, u, v):
+        '''
+        "Modified cost for logarithmic updates"
+        "$M_{ij} = (-c_{ij} + u_i + v_j) / epsilon$"
+        '''
+        return (-C + u.unsqueeze(-1) + v.unsqueeze(-2)) / self.eps
+
+
+# test time augmentation(TTA)
+class TestTimeAugmentation(object):
+    def __init__(self, num_classes=80, nms_thresh=0.4, scale_range=[320, 640, 32]):
+        self.nms = nms
+        self.num_classes = num_classes
+        self.nms_thresh = nms_thresh
+        self.scales = np.arange(scale_range[0], scale_range[1]+1, scale_range[2])
+        
+    def __call__(self, x, model):
+        # x: Tensor -> [B, C, H, W]
+        bboxes_list = []
+        scores_list = []
+        labels_list = []
+
+        # multi scale
+        for s in self.scales:
+            if x.size(-1) == s and x.size(-2) == s:
+                x_scale = x
+            else:
+                x_scale =torch.nn.functional.interpolate(
+                                        input=x, 
+                                        size=(s, s), 
+                                        mode='bilinear', 
+                                        align_corners=False)
+            model.set_grid(s)
+            bboxes, scores, labels = model(x_scale)
+            bboxes_list.append(bboxes)
+            scores_list.append(scores)
+            labels_list.append(labels)
+
+            # Flip
+            x_flip = torch.flip(x_scale, [-1])
+            bboxes, scores, labels = model(x_flip)
+            bboxes = bboxes.copy()
+            bboxes[:, 0::2] = 1.0 - bboxes[:, 2::-2]
+            bboxes_list.append(bboxes)
+            scores_list.append(scores)
+            labels_list.append(labels)
+
+        bboxes = np.concatenate(bboxes_list)
+        scores = np.concatenate(scores_list)
+        labels = np.concatenate(labels_list)
+
+        # nms
+        keep = np.zeros(len(bboxes), dtype=np.int)
+        for i in range(self.num_classes):
+            inds = np.where(labels == i)[0]
+            if len(inds) == 0:
+                continue
+            c_bboxes = bboxes[inds]
+            c_scores = scores[inds]
+            c_keep = self.nms(c_bboxes, c_scores, self.nms_thresh)
+            keep[inds[c_keep]] = 1
+
+        keep = np.where(keep > 0)
+        bboxes = bboxes[keep]
+        scores = scores[keep]
+        labels = labels[keep]
+
+        return bboxes, scores, labels
